c9749221eedae25dbb5b903b3c6bb021b58520f2
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-08-26 12:39:12 +0200

    started website

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 #use this to start the website
+from website import create_app
 
+app = create_app()
 
+if __name__ == "__main__":
+    # run website in debug mode (automatically reruns server whenever you change anything)
+    app.run(debug = True)
