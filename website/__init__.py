8cdef83534137a7205f14d9161602fedb1389630
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-08-26 13:44:56 +0200

    Added home page

--- website/__init__.py
+++ website/__init__.py
@@ -2,9 +2,17 @@
 from flask import Flask
 
 def create_app():
-    # Initiallize app
-    app = Flask(__name__) # __name__ represents the name of the file
+    """Initialize the website"""
+    app = Flask(__name__) # __name__ represents the name of the application's module or package
     with open("secret_key.txt", "r") as f:
-        app.config['SECRET_KEY'] = f #encrypts the cookies/session data
-    return app
-    
+        app.config['SECRET_KEY'] = f.read() #encrypts the cookies/session data
+
+    # Import blueprints
+    from .views import views
+    from .auth import auth
+
+    # Register blueprints
+    app.register_blueprint(views, url_prefix = '/')
+    app.register_blueprint(auth, url_prefix = '/')
+
+    return app    
--- website/views.py
+++ website/views.py
@@ -1,1 +1,10 @@
 #URL end points/front end aspects of the website
+from flask import Blueprint # lets us define views in multiple fles
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    """This function will run when we go to the root directory of the
+    website"""
+    return "<h1>Test</h1>"
