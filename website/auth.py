f6c21e170032103302547a4d44d08fc6600100fc
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-08-26 13:49:14 +0200

    added login out and sign up pages

--- website/auth.py
+++ website/auth.py
@@ -1,1 +1,16 @@
 #authorization
+from flask import Blueprint # lets us define views in multiple fles
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
