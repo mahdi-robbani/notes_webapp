82fb33c7e97855073116660e85e2b86e913d3fd3
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-08-28 20:04:53 +0200

    Added account sign up

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,11 @@ from flask import Blueprint # lets us define views in multiple files
 from flask import render_template # lets us render HTMLS
 from flask import request
 from flask import flash # show message to user
+from flask import redirect
+from flask import url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -19,23 +24,31 @@ def logout():
 @auth.route('/sign-up', methods = ["GET", "POST"])
 def sign_up():
     if request.method == "POST":
-        # get form info
+        # Get form info
         email = request.form.get("email")
-        firstName = request.form.get("firstName")
+        first_name = request.form.get("firstName")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
         
-        #checks
+        # Checks
         if len(email) < 5:
             flash("Email must contain at least 5 characters.", category="error")
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("First name must contain at least 2 characters.", category="error")
         elif password1 != password2:
             flash("Passwords do not match.", category="error")
         elif len(password1) < 6:
             flash("Password must contain at least 6 characters.", category="error")
         else:
+            # Add user to database
+            new_user = User(email=email, 
+                            first_name=first_name, 
+                            password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            # Show message
             flash("Account created!", category="success")
-            #add user to database
+            # Redirect to homepage
+            return redirect(url_for('views.home')) # find which url maps to the page home (under views.py)
 
-    return  render_template("sign_up.html")
+    return render_template("sign_up.html")
--- website/models.py
+++ website/models.py
@@ -16,6 +16,6 @@ class User(db.Model, UserMixin):
     """Class which represents the User table in the database"""
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
+    first_name = db.Column(db.String(100))
     password = db.Column(db.String(100))
-    firstName = db.Column(db.String(100))
     notes = db.relationship('Note') # allows us to access all the notes of a user
