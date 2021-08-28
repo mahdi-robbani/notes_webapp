a63e967ae9b9de1423f1ed8525d27e51afcc2286
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-08-28 19:37:59 +0200

    Added database

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,22 @@
 # make website folder a python package
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+# create database
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     """Initialize the website"""
+
     app = Flask(__name__) # __name__ represents the name of the application's module or package
     with open("secret_key.txt", "r") as f:
         app.config['SECRET_KEY'] = f.read() #encrypts the cookies/session data
+    # Let flask know we are using a database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    # Initialize datase with our website
+    db.init_app(app)
 
     # Import blueprints
     from .views import views
@@ -15,4 +26,17 @@ def create_app():
     app.register_blueprint(views, url_prefix = '/')
     app.register_blueprint(auth, url_prefix = '/')
 
+    # Classes need to be defined before initializing db
+    from .models import User, Note
+    create_database(app)
+
     return app    
+
+def create_database(app):
+    """Checks if database already exists for website and creates a
+    database if it does not already exist"""
+
+    if not path.exists(f"website/{DB_NAME}"):
+        db.create_all(app=app)
+        print("Created Database!")
+
--- website/models.py
+++ website/models.py
@@ -1,1 +1,21 @@
-#store database models
+# Store database models
+from . import db # import database from __init__
+from flask_login import UserMixin # Custom class for our User object
+from sqlalchemy.sql import func # Let sql handle the date
+
+class Note(db.Model):
+    """Class which represents the Note table in the database"""
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(10000))
+    # func.now lets sql use the current time
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    """Class which represents the User table in the database"""
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    firstName = db.Column(db.String(100))
+    notes = db.relationship('Note') # allows us to access all the notes of a user
