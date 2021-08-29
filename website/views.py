29b26d513d4c59be5307df3771e2c79e20718283
Mahdi Robbani <mahdi.robbani@gmail.com>
2021-08-29 13:22:48 +0200

    Added functionality to add notes

--- website/views.py
+++ website/views.py
@@ -2,12 +2,28 @@
 from flask import Blueprint # lets us define views in multiple files
 from flask import render_template # lets us render HTMLS
 from flask_login import login_required, current_user
+from flask import request
+from flask import flash
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods = ["GET", "POST"])
 @login_required
 def home():
-    """This function will run when we go to the root directory of the
-    website"""
+    """Home page of website displays Notes. Only accessible on login"""
+
+    if request.method == "POST":
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash("Note must have at least one character.", category="error")
+        else:
+            # Add note to database based on user id
+            new_note = Note(text=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note added!", category="success")
+
     return render_template("home.html", user=current_user) # user current user in the home template
